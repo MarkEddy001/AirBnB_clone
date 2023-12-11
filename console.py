7a845d5bc12e18ca5012797297a715f4a660e367
MarkEddy001 <Markeddy254@gmail.com>
2023-12-11 01:42:00 -0800

    Update the console.py

--- console.py
+++ console.py
@@ -22,14 +22,14 @@ def parse(arg):
     brackets = re.search(r"\[(.*?)\]", arg)
     if curly_braces is None:
         if brackets is None:
-            return [i.strip(",") for i in split(arg)]
+            return [i.strip(",") for i in arg.split()]
         else:
-            lexer = split(arg[:brackets.span()[0]])
+            lexer = arg[:brackets.span()[0]].split()
             retl = [i.strip(",") for i in lexer]
             retl.append(brackets.group())
             return retl
     else:
-        lexer = split(arg[:curly_braces.span()[0]])
+        lexer = arg[:curly_braces.span()[0]].split()
         retl = [i.strip(",") for i in lexer]
         retl.append(curly_braces.group())
         return retl
@@ -50,6 +50,14 @@ class HBNBCommand(cmd.Cmd):
         "Review"
     }
 
+    def emptyline(self):
+        """Do nothing upon receiving an empty line."""
+        pass
+
+    def default(self, arg):
+        """
+        Default behavior for cmd module when input is invalid
+        """
     def emptyline(self):
         """Do nothing upon receiving an empty line.
         """
